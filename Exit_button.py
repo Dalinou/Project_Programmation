8ddab0ef6a8b1204b525af5706685c792dfdfe14
arthur <arthur.pataille@gmail.com>
2022-05-03 17:13:55 +0200

    Rajout du bouton exit (fonctionnel) et déplacement de la position des boutons. Exit_button.py sera modifié pour mettre un avertissement si une sauvegarde n'a pas été faite dans les 5min avant de quitter

--- Exit_button.py
+++ Exit_button.py
@@ -6,26 +6,8 @@ import button
 
 class Exit:
     def __init__(self, window, clock, setting):
-        #paramétrage de l'écran
-        self.window = window
-        self.clock = clock
-        self.setting = setting
-        self.font = pygame.font.Font("Game_font.TTF", 48)
 
-        #Chargements des textures
-        self.texture_background = self.setting.get_texture("Texture/Background.png")
 
-        #button exit
-        self.button_exit = button.Button(
-            [self.setting.screensize[0] / 2, self.setting.screensize[1] * 3 / 4],
-            2,
-            0,
-            ["Texture/Button up.png", "Texture/Button down.png"],
-            self.font,
-            ["Exit", "Exit"],
-            [pygame.Color("#CB4F00"), pygame.Color("#FE6400")],
-            screensize_adaption=True, screensize=self.setting.screensize
-        )
 
     def gameloop(self):
         x = True
@@ -54,13 +36,3 @@ class Exit:
                     x = False
                 else:
                     print("Veuillez répondre 'y' ou 'n'")
-
-#### Mettre ce qu'il y a au dessus dans menu.py -> gameloop : sur le pygame.QUIT:
-
-#### + rajouter : # regarde si la souris est sur le bouton exit
-                    if self.button_exit.is_coord_on(self.cursor_coord):
-                        self.button_exit.set_state(1)
-
-                    if self.button_back.state == 1:
-                        # renvoies la valeur permettant de quitter
-                        return "exit"
--- menu.py
+++ menu.py
@@ -19,7 +19,7 @@ class Menu:
         self.texture_background = self.setting.get_texture("Texture/Background.png")
         # Chargement des boutons
         self.button_continue = button.Button(
-            [self.setting.screensize[0] / 2, self.setting.screensize[1] * 2 / 5],
+            [self.setting.screensize[0] * 1 / 5, self.setting.screensize[1] * 2 / 5],
             3,
             0 if os.path.exists("save.txt") else 2,
             ["Texture/Button up.png", "Texture/Button down.png", "Texture/Button gray.png"],
@@ -29,7 +29,7 @@ class Menu:
             screensize_adaption=True, screensize=self.setting.screensize
         )
         self.button_new_game = button.Button(
-            [self.setting.screensize[0] / 2, self.setting.screensize[1] * 3 / 5],
+            [self.setting.screensize[0] * 1 / 5, self.setting.screensize[1] * 4 / 5],
             2,
             0,
             ["Texture/Button up.png", "Texture/Button down.png"],
@@ -39,7 +39,7 @@ class Menu:
             screensize_adaption=True, screensize=self.setting.screensize
         )
         self.button_setting = button.Button(
-            [self.setting.screensize[0] / 2, self.setting.screensize[1] * 4 / 5],
+            [self.setting.screensize[0] * 4 / 5, self.setting.screensize[1] * 2 / 5],
             2,
             0,
             ["Texture/Button up.png", "Texture/Button down.png"],
@@ -48,6 +48,17 @@ class Menu:
             [pygame.Color("#CB4F00"), pygame.Color("#FE6400")],
             screensize_adaption=True, screensize=self.setting.screensize
         )
+        self.button_exit = button.Button(
+            [self.setting.screensize[0] * 4/5, self.setting.screensize[1] * 4 / 5],
+            2,
+            0,
+            ["Texture/Button up.png", "Texture/Button down.png"],
+            self.button_font,
+            ["Exit", "Exit"],
+            [pygame.Color("#CB4F00"), pygame.Color("#FE6400")],
+            screensize_adaption=True, screensize=self.setting.screensize
+        )
+
         # Curseur
         self.texture_cursor = pygame.image.load("Texture/Cursor.png")
         # Coordonnée des différents objets
@@ -84,6 +95,12 @@ class Menu:
                         self.button_setting.set_state(1)
                     else:
                         self.button_setting.set_state(0)
+                    # regarde si la souris est sur le bouton exit
+                    if self.button_exit.is_coord_on(self.cursor_coord):
+                        self.button_exit.set_state(1)
+                    else:
+                        self.button_exit.set_state(0)
+
                 # Si Click de la souris
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     if self.button_continue.state == 1:
@@ -95,6 +112,8 @@ class Menu:
                             return "warning"
                     if self.button_setting.state == 1:
                         return "paramètre"
+                    if self.button_exit.state == 1:
+                        return "exit"
 
             # Affichage du fond d'écran
             self.window.blit(self.texture_background, (0, 0))
@@ -111,6 +130,7 @@ class Menu:
             self.button_continue.render(self.window)
             self.button_new_game.render(self.window)
             self.button_setting.render(self.window)
+            self.button_exit.render(self.window)
             #  Affichage du curseur
             self.window.blit(self.texture_cursor, self.cursor_coord)
             # Actualisation de l'affichage
