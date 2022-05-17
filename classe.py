b1d2d22ff048c24c80a816d12314dbaa16c01126
Gwendal <gwendal.braun@gmail.com>
2022-05-17 16:19:23 +0200

    Avec othilie, modification et harmonisation, ajout d'une fonction pour lire classe.json

--- classe.py
+++ classe.py
@@ -1,7 +1,16 @@
 import json
+# pour recupérer les classes
+''' 
+# Ouverture du fichier
+with open("classe.json") as data:
+    # Ouverture du fichier à l'aide du décodeur json et décoder classe.decode
+    z = json.load(data, object_hook=classe.decode)
+# sert à désigner chaque classe par son nom, non un numéro (pas forcément fixe)
+z = {z[i].classe_name: z[i] for i in range(z.__len__())}
+'''
 
 
-class classe:
+class Classe:
     def __init__(self, classe_name, defense, atk, pv, description, atk_type):
         self.classe_name = classe_name
         self.defense = defense
@@ -13,6 +22,6 @@ class classe:
 
 def decode(dct):
     if "__classe__" in dct:
-        return classe(dct["classe name"], dct["def"], dct["atk"], dct["pv"], dct["description"], dct["atk_type"])
+        return Classe(dct["classe name"], dct["def"], dct["atk"], dct["pv"], dct["description"], dct["atk type"])
     return dct
 
--- personnage.py
+++ personnage.py
@@ -1,12 +1,24 @@
 class Personnage:
     def __init__(self, data):
         if "__personnage__" in data:
-            self.classe_type = data["classe type"]
+            self.classe = data["classe"]
             self.gender = data["gender"]
             self.pv = data["pv"]
-            self.pv_max = data["pv_max"]
+            self.pv_max = data["pv max"]
             self.atk = data["atk"]
             self.defense = data["def"]
+
+    def save(self):
+        return{
+            "__personnage__": True,
+            "classe": self.classe,
+            "gender": self.gender,
+            "pv": self.pv,
+            "pv max": self.pv_max,
+            "atk": self.atk
+
+        }
+
 
 
 
