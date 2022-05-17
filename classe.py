4fb2111ea2fb29a3bed9ddfca4b8814ee84597ad
Gwendal <gwendal.braun@gmail.com>
2022-05-17 17:28:22 +0200

    Ajout de texture comme donnée des classes

--- classe.py
+++ classe.py
@@ -11,17 +11,19 @@ z = {z[i].classe_name: z[i] for i in range(z.__len__())}
 
 
 class Classe:
-    def __init__(self, classe_name, defense, atk, pv, description, atk_type):
+    def __init__(self, classe_name, defense, atk, pv, description, atk_type, texture):
         self.classe_name = classe_name
         self.defense = defense
         self.atk = atk
         self.pv = pv
         self.description = description
         self.atk_type = atk_type
+        self.texture = texture
 
 
 def decode(dct):
     if "__classe__" in dct:
-        return Classe(dct["classe name"], dct["def"], dct["atk"], dct["pv"], dct["description"], dct["atk type"])
+        return Classe(dct["classe name"], dct["def"], dct["atk"], dct["pv"],
+                      dct["description"], dct["atk type"], dct["texture"])
     return dct
 
