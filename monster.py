a4bce134b0e00818ad9ffb6c4426164f84e02e8f
Lasra <othilie.rotagraziosi@gmail.com>
2022-06-02 16:36:44 +0200

    Modification de la carte & avancement de la classe monstre pour récuperer les données de chaque monstre

--- monster.py
+++ monster.py
@@ -1,19 +1,24 @@
+import json
+
 import pygame
 
 
 # classe qui gère les statistiques des monstres ainsi que leur mouvements
 class Monster:
-    def __init__(self, name, pv, atk, defense, texture):
+    def __init__(self, data):
         # création des variables pour les statistiques des monstres + leur texture
-        self.name = name
-        self.pv = pv
-        self.atk = atk
-        self.defense = defense
-        self.texture = pygame.image.load(texture)
+        if "__monster__" in data:
+            self.name = data["name"]
+            self.pv = data["pv"]
+            self.atk = data["atk"]
+            self.defense = data["defense"]
+            self.texture = data["pygame.image.load(texture)"]
 
-    # permet de récuperer les informations contenus dans le json
-    def decode(dct):
-        if "__monster__" in dct:
-            return Monster(dct["name"], dct["def"], dct["atk"], dct["pv"],
-                           dct["texture"])
-        return dct
+    def monster(type):
+        # look in monster.json
+        with open("monster.json") as data:
+            z = json.load(data)
+            z = {z[i]["type"]: z[i] for i in range(z.__len__())}
+            if type in z:
+                data = z[type]
+                return Monster(data)
