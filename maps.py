6694751b8d87b9ef931acbae0e0639f988b9bbf3
Gwendal <gwendal.braun@gmail.com>
2022-06-09 17:37:26 +0200

    Modification et homogénéisation du code

--- maps.py
+++ maps.py
@@ -5,10 +5,10 @@ import pygame.image
 # TODO Commentaire !!!
 # Classe Maps sert à stocké les informations de cartes, elle fait aussi la gestion de son affichage
 class Maps:
-    def __init__(self, filename, coord_init, setting):
+    def __init__(self, filename, init_location, setting):
         self.setting = setting
         self.filename = filename
-        self.coord = coord_init
+        self.location = init_location
         self.map = {}
         self.tile = {}
         self.tilesize = None
@@ -18,23 +18,23 @@ class Maps:
     # Fonction pour fait le rendu de la carte, centré autour du perso
     def render(self, window, sprite_list):
         # selectionne la carte à utilisé
-        map_ = self.map[self.coord[2]]
+        map_ = self.map[self.location[2]]
         # définit l'offset minimun par rapport au bord avant de bougé
         off = [3, 3]
         # définit les coordonées de la case en (0, 0)
-        coord0 = [self.setting.screensize[0] / 2 - map_.res[0] * self.tilesize[0] / 2,
+        location0 = [self.setting.screensize[0] / 2 - map_.res[0] * self.tilesize[0] / 2,
                   self.setting.screensize[1] / 2 - map_.res[1] * self.tilesize[1] / 2]
 
-        # vérifie si coord0 respecte l'offset et le rectifie si besoin
-        if coord0[0] + self.tilesize[0] * (self.coord[0] - off[0]) < 0:
-            coord0[0] = 0 - (0 if self.coord[0] <= off[0] else self.coord[0] - 3) * self.tilesize[0]
-        elif coord0[0] + self.tilesize[0] * (self.coord[0] + off[0] + 1) >= self.setting.screensize[0]:
-            coord0[0] = max(self.setting.screensize[0] / self.tilesize[0] - self.coord[0] - off[0] - 1,
+        # vérifie si location0 respecte l'offset et le rectifie si besoin
+        if location0[0] + self.tilesize[0] * (self.location[0] - off[0]) < 0:
+            location0[0] = 0 - (0 if self.location[0] <= off[0] else self.location[0] - 3) * self.tilesize[0]
+        elif location0[0] + self.tilesize[0] * (self.location[0] + off[0] + 1) >= self.setting.screensize[0]:
+            location0[0] = max(self.setting.screensize[0] / self.tilesize[0] - self.location[0] - off[0] - 1,
                             self.setting.screensize[0] / self.tilesize[0] - map_.res[0]) * self.tilesize[0]
-        if coord0[1] + self.tilesize[1] * (self.coord[1] - off[1]) < 0:
-            coord0[1] = 0 - (0 if self.coord[1] <= off[1] else self.coord[1] - 3) * self.tilesize[1]
-        elif coord0[1] + self.tilesize[1] * (self.coord[1] + off[1] + 1) >= self.setting.screensize[1]:
-            coord0[1] = max(self.setting.screensize[1]/self.tilesize[1] - self.coord[1] - off[1] - 1,
+        if location0[1] + self.tilesize[1] * (self.location[1] - off[1]) < 0:
+            location0[1] = 0 - (0 if self.location[1] <= off[1] else self.location[1] - 3) * self.tilesize[1]
+        elif location0[1] + self.tilesize[1] * (self.location[1] + off[1] + 1) >= self.setting.screensize[1]:
+            location0[1] = max(self.setting.screensize[1]/self.tilesize[1] - self.location[1] - off[1] - 1,
                             self.setting.screensize[1] / self.tilesize[1] - map_.res[1]) * self.tilesize[1]
         # affiche le fond d'écran
         window.blit(map_.background, (0, 0))
@@ -42,12 +42,12 @@ class Maps:
         for i in range(0, map_.res[1]):
             for j in range(0, map_.res[0]):
                 window.blit(self.tile[map_.map[i][j]].texture,
-                            [j * self.tilesize[0] + coord0[0], i * self.tilesize[1] + coord0[1]])
+                            [j * self.tilesize[0] + location0[0], i * self.tilesize[1] + location0[1]])
         # affichage de la liste de sprite donnée, un sprite doit avoir comme donnée : une coordonée et une texture
         for el in sprite_list:
-            if el.coord[2] == self.coord[2] and 0 <= el.coord[0] < map_.res[0] and 0 <= el.coord[1] < map_.res[1]:
-                window.blit(el.texture, [self.tilesize[0] * el.coord[0] + coord0[0],
-                                         self.tilesize[1] * el.coord[1] + coord0[1]])
+            if el.location[2] == self.location[2] and 0 <= el.location[0] < map_.res[0] and 0 <= el.location[1] < map_.res[1]:
+                window.blit(el.texture, [self.tilesize[0] * el.location[0] + location0[0],
+                                         self.tilesize[1] * el.location[1] + location0[1]])
 
     # Fonction pour lire le maps.json
     def decode(self):
@@ -89,6 +89,6 @@ class Tile:
 
 # class type pour ce qu'il faut comment attribut
 class Sprite:
-    def __init__(self, coord, texture):
-        self.coord = coord
+    def __init__(self, location, texture):
+        self.location = location
         self.texture = texture
