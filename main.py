a0dab2a47d55f9aeb8e4434a15270ae405375fae
Gwendal <gwendal.braun@gmail.com>
2022-06-22 17:34:57 +0200

    Création d'une nouvelle fork + ajout d'un if __name__ == "__main__"

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import menu
 import settings
 import create_perso
 import game
-
 '''
 Projet de Programmation 2022
 Auteurs: Braun Gwendal, Rota-Graziosi Othilie, Pataille Arthur
@@ -23,55 +22,64 @@ Les classes monster.Monster et personnage. Personnage définit les objects "joue
 Enfin maps.py fait la gestion de la carte
 Toutes les données sont sauvegardées dans des fichiers en .json
 '''
-# initiation de pygame
-pygame.font.init()
-pygame.init()
-# Mise en invisible du curseur de windows
-pygame.mouse.set_visible(False)
-# changement du nom de la fenêtre et de l'icone
-pygame.display.set_caption("RPG ADVENTURE")
-pygame.display.set_icon(pygame.image.load("Texture/Game Icon.png"))
 
-# Paramètre
-setting = settings.SettingReader()
 
-# Création de la fenetre
-if setting.fullscreen:
-    window = pygame.display.set_mode(setting.fullscreen_resolution, pygame.FULLSCREEN)
-else:
-    window = pygame.display.set_mode(setting.screensize)
-clock = pygame.time.Clock()
+def __main__():
+    # initiation de pygame
+    pygame.font.init()
+    pygame.init()
+    # Mise en invisible du curseur de windows
+    pygame.mouse.set_visible(False)
+    # changement du nom de la fenêtre et de l'icone
+    pygame.display.set_caption("RPG ADVENTURE")
+    pygame.display.set_icon(pygame.image.load("Texture/Game Icon.png"))
 
-# Ecran à affiché / Etat du jeu
-state = "menu"
+    # Paramètre
+    setting = settings.SettingReader()
 
-while state != "exit":
-    setting.read_file()
-    # regarde l'état du jeu et lance la gameloop voulu en fonction de cet etat
-    # Fenêtre menu
-    if state == "menu":
-        screen = menu.Menu(window, clock, setting)
-        state = screen.gameloop()
-    # Fenêtre création du personnage
-    elif state == "create":
-        screen = create_perso.CreatePerso(window, clock, setting)
-        state = screen.gameloop()
-    # Fenêtre warning lorsqu'une sauvegarde existe
-    elif state == "warning":
-        screen = create_perso.CreateWarning(window, clock, setting)
-        state = screen.gameloop()
-    # Fenêtre paramètres
-    elif state == "paramètre":
-        screen = settings.SettingScreen(window, clock, setting)
-        state = screen.gameloop()
-    # Fenêtre du jeu
-    elif state == "game":
-        screen = game.GameScreen(window, clock, setting)
-        state = screen.gameloop()
-    # Fenêtre de fin du jeu si le perso meurt
-    elif state == "game_over":
-        screen = game.GameOver(window, clock, setting)
-        state = screen.gameloop()
-    # si l'état pour une raison quelconque a une valeur inconnu, on retourne au menu
+    # Création de la fenetre
+    if setting.fullscreen:
+        window = pygame.display.set_mode(setting.fullscreen_resolution, pygame.FULLSCREEN)
     else:
-        state = "menu"
+        window = pygame.display.set_mode(setting.screensize)
+    clock = pygame.time.Clock()
+
+    # Ecran à affiché / Etat du jeu
+    state = "menu"
+
+    # Boucle de jeu, reste dans la boucle temps qu'on ne demande pas de sortir du jeu
+    while state != "exit":
+        setting.read_file()
+        # regarde l'état du jeu et lance la gameloop voulu en fonction de cet etat
+        # Fenêtre menu
+        if state == "menu":
+            screen = menu.Menu(window, clock, setting)
+            state = screen.gameloop()
+        # Fenêtre création du personnage
+        elif state == "create":
+            screen = create_perso.CreatePerso(window, clock, setting)
+            state = screen.gameloop()
+        # Fenêtre warning lorsqu'une sauvegarde existe
+        elif state == "warning":
+            screen = create_perso.CreateWarning(window, clock, setting)
+            state = screen.gameloop()
+        # Fenêtre paramètres
+        elif state == "paramètre":
+            screen = settings.SettingScreen(window, clock, setting)
+            state = screen.gameloop()
+        # Fenêtre du jeu
+        elif state == "game":
+            screen = game.GameScreen(window, clock, setting)
+            state = screen.gameloop()
+        # Fenêtre de fin du jeu si le perso meurt
+        elif state == "game_over":
+            screen = game.GameOver(window, clock, setting)
+            state = screen.gameloop()
+        # si l'état pour une raison quelconque a une valeur inconnu, on retourne au menu
+        else:
+            state = "menu"
+
+
+if __name__ == "__main__":
+    __main__()
+
